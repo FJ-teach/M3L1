37c4d59a06d656f11873ee4f2df8c0e9008c90d8
fj <fj@afrisecgroup.co.za>
2023-04-11 13:43:03 +0200

    commit images to memory

--- routes/pictures.js
+++ routes/pictures.js
@@ -2,17 +2,53 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 var path = require('path');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
 /* GET pictures listing. */
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
   // res.render('pictures');
-  const pictures = fs.readdirSync(path.join(__dirname, '../public/pictures/'));
-  res.render('pictures', { pictures: pictures });
+  // const pictures = fs.readdirSync(path.join(__dirname, '../public/pictures/'));
+  // res.render('pictures', { pictures: pictures });
+  var params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Delimiter: '/',
+    Prefix: 'public/',
+  };
+
+  var allObjects = await s3.listObjects(params).promise();
+  var keys = allObjects?.Contents.map((x) => x.Key);
+  const pictures = await Promise.all(
+    keys.map(async (key) => {
+      let my_file = await s3
+        .getObject({
+          Bucket: process.env.CYCLIC_BUCKET_NAME,
+          Key: key,
+        })
+        .promise();
+
+      return {
+        src: Buffer.from(my_file.Body).toString('base64'),
+        name: key.split('/').pop(),
+      };
+    })
+  );
+
+  res.render('pictures', { pictures: pictures, title: 'Express' });
 });
 
-router.post('/', function (req, res, next) {
+router.post('/', async function (req, res, next) {
+  const file = req.files.file;
+
   console.log(req.files);
+  await s3
+    .putObject({
+      Body: file.data,
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Key: 'public/' + file.name,
+    })
+    .promise();
   res.end();
 });
 
